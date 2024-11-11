1049b6051c5deb8a145cb2a1ddd3d093dd19bd7a
artyom <ryazancev_0101@mail.ru>
2024-11-11 09:45:53 +0300

    1 dz

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +16,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val button1 = findViewById<Button>(R.id.firstbutton)
+        val button2 = findViewById<Button>(R.id.secondbutton)
+        val button3 = findViewById<Button>(R.id.thirdbutton)
+
+
+        button1.setOnClickListener {
+            val displayIntent = Intent(this, Search::class.java)
+            startActivity(displayIntent)
+        }
+
+        button2.setOnClickListener {
+            val displayIntent = Intent(this, Mediateka::class.java)
+            startActivity(displayIntent)
+        }
+
+        button3.setOnClickListener {
+            val displayIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(displayIntent)
+        }
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
